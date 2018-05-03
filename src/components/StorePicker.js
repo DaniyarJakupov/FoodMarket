03e9571f334d03a6d0ada66d809f5249dc97cb44
DaniyarJakupov <ddr199305@gmail.com>
2018-05-03 23:17:50 +0200

    Remove cra boilerplate

---
+++ src/components/StorePicker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StorePicker = () => (
+  <div>
+    <form action="">
+      <input type="text" name="name" />
+      <button>Submit</button>
+    </form>
+  </div>
+);
+
+export default StorePicker;
