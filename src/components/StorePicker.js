37e9ab75f143d2d11dafa53ccbf95242c48b1ae0
DaniyarJakupov <ddr199305@gmail.com>
2018-05-05 13:17:26 +0200

    Add css

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 const StorePicker = () => (
-  <div>
-    <form action="">
-      <input type="text" name="name" />
-      <button>Submit</button>
-    </form>
-  </div>
+  <form action="" className="store-selector">
+    <h2>Enter A Store</h2>
+    <input type="text" placeholder="Store Name" />
+    <button type="submit">Submit</button>
+  </form>
 );
 
 export default StorePicker;
