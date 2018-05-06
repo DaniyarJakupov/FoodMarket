fa0814ae5a5227b2b968efe2a5172e35588bf2ef
DaniyarJakupov <ddr199305@gmail.com>
2018-05-06 22:52:29 +0200

    Add auth functionality

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import firebase from 'firebase';
+import { withRouter } from 'react-router-dom';
 
 import NewDishForm from './NewDishForm';
 import EditDishForm from './EditDishForm';
@@ -8,8 +9,35 @@ import Login from './Login';
 import base, { firebaseApp } from '../utils/base';
 
 class Inventory extends Component {
+  state = {
+    uid: null,
+    owner: null
+  };
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.authHandler({ user });
+      }
+    });
+  }
+
   authHandler = async authData => {
-    console.log(authData);
+    // 1. Find current store in firebase
+    const store = await base.fetch(this.props.match.params.storeId, {
+      context: this
+    });
+    // 2. Assign store to the currently logged in user if there is no owner
+    if (!store.owner) {
+      await base.post(`${this.props.match.params.storeId}/owner`, {
+        data: authData.user.uid
+      });
+    }
+    // 3. Save owner info and current user info
+    this.setState({
+      uid: authData.user.uid,
+      owner: store.owner || authData.user.uid
+    });
   };
 
   authenticate = provider => {
@@ -22,27 +50,47 @@ class Inventory extends Component {
       .then(this.authHandler);
   };
 
+  logout = async () => {
+    await firebase.auth().signOut();
+    this.setState({
+      uid: null,
+      owner: null
+    });
+  };
+
   render() {
     const { dishes, updateDish, deleteDish, addDish, loadSamples } = this.props;
-    return <Login authenticate={this.authenticate} />;
+    const logout = <button onClick={this.logout}>Logout</button>;
+
+    if (!this.state.uid) {
+      return <Login authenticate={this.authenticate} />;
+    }
+    if (this.state.uid === this.state.owner) {
+      return (
+        <div className="inventory">
+          <h2>Inventory</h2>
+          {logout}
+          {Object.keys(dishes).map(key => {
+            return (
+              <EditDishForm
+                dish={dishes[key]}
+                key={key}
+                id={key}
+                updateDish={updateDish}
+                deleteDish={deleteDish}
+              />
+            );
+          })}
+          <NewDishForm addDish={addDish} />
+
+          <button onClick={loadSamples}>Load Sample Dishes</button>
+        </div>
+      );
+    }
     return (
-      <div className="inventory">
-        <h2>Inventory</h2>
-
-        {Object.keys(dishes).map(key => {
-          return (
-            <EditDishForm
-              dish={dishes[key]}
-              key={key}
-              id={key}
-              updateDish={updateDish}
-              deleteDish={deleteDish}
-            />
-          );
-        })}
-        <NewDishForm addDish={addDish} />
-
-        <button onClick={loadSamples}>Load Sample Dishes</button>
+      <div>
+        <p>You don't have access to this section</p>
+        {logout}
       </div>
     );
   }
@@ -56,4 +104,4 @@ Inventory.propTypes = {
   loadSamples: PropTypes.func.isRequired
 };
 
-export default Inventory;
+export default withRouter(Inventory);
