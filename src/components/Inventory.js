7e0e507f52601e1952e8ec2a903b9b2dff039c12
DaniyarJakupov <ddr199305@gmail.com>
2018-05-06 17:46:02 +0200

    Convert to functional component

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import NewDishForm from '../containers/NewDishForm';
 import EditDishForm from '../containers/EditDishForm';
 
-class Inventory extends Component {
-  state = {};
-  render() {
-    return (
-      <div className="inventory">
-        <h2>Inventory</h2>
+const Inventory = ({
+  dishes,
+  updateDish,
+  deleteDish,
+  addDish,
+  loadSamples
+}) => {
+  return (
+    <div className="inventory">
+      <h2>Inventory</h2>
 
-        {Object.keys(this.props.dishes).map(key => {
-          return (
-            <EditDishForm
-              dish={this.props.dishes[key]}
-              key={key}
-              id={key}
-              updateDish={this.props.updateDish}
-              deleteDish={this.props.deleteDish}
-            />
-          );
-        })}
-        <NewDishForm addDish={this.props.addDish} />
+      {Object.keys(dishes).map(key => {
+        return (
+          <EditDishForm
+            dish={dishes[key]}
+            key={key}
+            id={key}
+            updateDish={updateDish}
+            deleteDish={deleteDish}
+          />
+        );
+      })}
+      <NewDishForm addDish={addDish} />
 
-        <button onClick={this.props.loadSamples}>Load Sample Dishes</button>
-      </div>
-    );
-  }
-}
+      <button onClick={loadSamples}>Load Sample Dishes</button>
+    </div>
+  );
+};
 
 export default Inventory;
