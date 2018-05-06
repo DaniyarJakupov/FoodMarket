23db33a270f3762fac69be17eed9aacf0c5aba33
DaniyarJakupov <ddr199305@gmail.com>
2018-05-06 22:26:57 +0200

    Setup OAuth with firebase

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,37 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import firebase from 'firebase';
 
 import NewDishForm from './NewDishForm';
 import EditDishForm from './EditDishForm';
+import Login from './Login';
+import base, { firebaseApp } from '../utils/base';
 
-const Inventory = ({
-  dishes,
-  updateDish,
-  deleteDish,
-  addDish,
-  loadSamples
-}) => {
-  return (
-    <div className="inventory">
-      <h2>Inventory</h2>
+class Inventory extends Component {
+  authHandler = async authData => {
+    console.log(authData);
+  };
 
-      {Object.keys(dishes).map(key => {
-        return (
-          <EditDishForm
-            dish={dishes[key]}
-            key={key}
-            id={key}
-            updateDish={updateDish}
-            deleteDish={deleteDish}
-          />
-        );
-      })}
-      <NewDishForm addDish={addDish} />
+  authenticate = provider => {
+    // provider: Github, Twitter, Facebook
+    const authProvier = new firebase.auth[`${provider}AuthProvider`]();
 
-      <button onClick={loadSamples}>Load Sample Dishes</button>
-    </div>
-  );
-};
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvier)
+      .then(this.authHandler);
+  };
+
+  render() {
+    const { dishes, updateDish, deleteDish, addDish, loadSamples } = this.props;
+    return <Login authenticate={this.authenticate} />;
+    return (
+      <div className="inventory">
+        <h2>Inventory</h2>
+
+        {Object.keys(dishes).map(key => {
+          return (
+            <EditDishForm
+              dish={dishes[key]}
+              key={key}
+              id={key}
+              updateDish={updateDish}
+              deleteDish={deleteDish}
+            />
+          );
+        })}
+        <NewDishForm addDish={addDish} />
+
+        <button onClick={loadSamples}>Load Sample Dishes</button>
+      </div>
+    );
+  }
+}
 
 Inventory.propTypes = {
   dishes: PropTypes.object.isRequired,
